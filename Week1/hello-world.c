544894701103a6a97adfdad7db1aa1b0083242ed
Christian Salvador <41243724+KittenInAMitten@users.noreply.github.com>
2025-09-09 14:36:53 -0600

    Created hello-world.c

---
+++ Week1/hello-world.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * @brief This program is super simple and will be used to show you how to compile your very first C program!
+ * 
+ * Ensure you are logged into/SSH'd into the cslinux servers/computers.
+ * 
+ *  1. Complete the main function using printf().
+ *  2. Once done, type into the terminal: `gcc -O2 -Wall hello-world.c -o hello-world`
+ *  3. Type in `./hello-world`
+ *  4. Confirm the output says "hello world" or whatever you chose to output in the terminal
+ * 
+ * @return int 
+ */
+int main(){
+
+    // Just print "hello world" or whatever you want :)
+    return 0;
+}
